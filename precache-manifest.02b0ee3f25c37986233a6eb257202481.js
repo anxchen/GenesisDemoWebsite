8dfaef798c5e1e301bde241c379ee72da053cb05
anxchen <141370794+anxchen@users.noreply.github.com>
2025-10-30 20:55:40 -0700

    reconstruct the whole site for Genesis

---
+++ precache-manifest.02b0ee3f25c37986233a6eb257202481.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bb63648ce523620263b709f9d7f7a8",
+    "url": "/GenesisDemoWebsite/index.html"
+  },
+  {
+    "revision": "8c295df443894ab71cd4",
+    "url": "/GenesisDemoWebsite/static/css/2.9ff56b78.chunk.css"
+  },
+  {
+    "revision": "260eb32810f40f84ec00",
+    "url": "/GenesisDemoWebsite/static/css/main.e0dc36c2.chunk.css"
+  },
+  {
+    "revision": "8c295df443894ab71cd4",
+    "url": "/GenesisDemoWebsite/static/js/2.2c0972e2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/GenesisDemoWebsite/static/js/2.2c0972e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260eb32810f40f84ec00",
+    "url": "/GenesisDemoWebsite/static/js/main.84ac17cb.chunk.js"
+  },
+  {
+    "revision": "0683c7687455dcee2e23",
+    "url": "/GenesisDemoWebsite/static/js/runtime-main.22edee81.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/GenesisDemoWebsite/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "31b34024ab27deeb4342d4a8eed7f3f6",
+    "url": "/GenesisDemoWebsite/static/media/BlueDark_GenesisLogo.31b34024.png"
+  },
+  {
+    "revision": "b1e0586408c88d32a075c60da6515f87",
+    "url": "/GenesisDemoWebsite/static/media/BlueLight_GenesisLogo.b1e05864.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/GenesisDemoWebsite/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/GenesisDemoWebsite/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/GenesisDemoWebsite/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/GenesisDemoWebsite/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/GenesisDemoWebsite/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/GenesisDemoWebsite/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/GenesisDemoWebsite/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "523f833a8b5bdd6079b88981425e31a7",
+    "url": "/GenesisDemoWebsite/static/media/fa-brands-400.523f833a.woff2"
+  },
+  {
+    "revision": "ce76b7aa92724e57c94982ffbbc9a4a1",
+    "url": "/GenesisDemoWebsite/static/media/fa-regular-400.ce76b7aa.woff2"
+  },
+  {
+    "revision": "9209428ae208e223b94b9172802e97a7",
+    "url": "/GenesisDemoWebsite/static/media/fa-solid-900.9209428a.woff2"
+  },
+  {
+    "revision": "6b6b455b96ea52d70d69fb54e265803b",
+    "url": "/GenesisDemoWebsite/static/media/fa-v4compatibility.6b6b455b.woff2"
+  },
+  {
+    "revision": "dc75df84304aa6cbb377e7c37039bd41",
+    "url": "/GenesisDemoWebsite/static/media/moon.dc75df84.svg"
+  },
+  {
+    "revision": "4db24c0e662ca0fbacc25b3c959afedc",
+    "url": "/GenesisDemoWebsite/static/media/sun.4db24c0e.svg"
+  }
+]);
